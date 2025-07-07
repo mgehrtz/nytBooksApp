ffe73bbb12b370c70f15d5618c8cce680517d43c
Michael Gehrtz <63627744+mgehrtz@users.noreply.github.com>
2025-07-07 14:51:22 -0500

    Add seed file and Readme.

---
+++ README.md
@@ -0,0 +1,15 @@
+# NYT Best Sellers Full Stack App
+
+## Getting Started
+
+Build using:
+- Next.js
+- NestJS with Prisma
+
+To get up and runing, run the following commands:
+```shell
+git clone https://github.com/mgehrtz/nytBooksApp.git
+cd nytBestSellers
+npm install
+npm run stack
+```
---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+async function main() {
+  await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      firstName: 'Alice',
+      lastName: 'User',
+    },
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
